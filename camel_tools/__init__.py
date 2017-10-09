e6fb502e875bcc7b1871cbb83e739fe0b2024944
Ossama W. Obeid <owo@owobeid.com>
2017-10-09 18:46:03 +0400

    Added boilerplate for disambiguation script (camel_disambig).

---
+++ camel_tools/__init__.py
@@ -0,0 +1,16 @@
+"""
+A suite of morphological analysis and disambiguation tools for Arabic developed
+by the CAMeL Lab at New York University Abu Dhabi.
+"""
+
+from __future__ import print_function, absolute_import
+
+import os
+
+
+try:
+    version_file = os.path.join(os.path.dirname(__file__), 'VERSION')
+    with open(version_file, 'r') as infile:
+        __version__ = infile.read().strip()
+except Exception:
+    __version__ = '???'
