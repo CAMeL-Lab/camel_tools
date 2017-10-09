7ddd09960f2cbc245c90e989e2115dbf21e638d0
Ossama W. Obeid <owo@owobeid.com>
2017-10-09 15:26:09 +0400

    Added more pypi classifiers.

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 from setuptools import setup
 
 CLASSIFIERS = [
+    'Development Status :: 1 - Planning',
+    'Environment :: Console',
+    'Natural Language :: Arabic',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Information Technology',
+    'Intended Audience :: Science/Research',
+    'Operating System :: OS Independent',
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+    'Topic :: Text Processing',
+    'Topic :: Text Processing :: Linguistic',
 ]
 
 DESCRIPTION = ('A suite of morphological analysis and disambiguation tools '
