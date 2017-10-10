0524df45838203c08e87220545355b1e2ddc7236
Ossama W. Obeid <owo@owobeid.com>
2017-10-10 10:49:35 +0400

    Simplified project structure.

--- camel_tools/disambig/camel_disambig.py
+++ camel_tools/disambig/camel_disambig.py
@@ -14,23 +14,16 @@ Options:
 
 from __future__ import print_function, absolute_import
 
-import os
 import sys
 from docopt import docopt
 import camel_tools as camelt
 
 
-try:
-    version_file = os.path.join(os.path.dirname(__file__), 'VERSION')
-    with open(version_file, 'r') as infile:
-        __version__ = infile.read().strip()
-except Exception:
-    __version__ = '???'
+__version__ = camelt.__version__
 
 
-def main():
-    version = ('camel_disambig v{} | '
-               'CAMeL Tools v{}'.format(__version__, camelt.__version__))
+def main():  # pragma: no cover
+    version = ('CAMeL Tools v{}'.format(__version__))
     arguments = docopt(__doc__, version=version)
 
     # TODO: Implement camel_disambig
@@ -38,5 +31,5 @@ def main():
     sys.exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
     main()
