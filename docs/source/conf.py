2e4a6d2ea70fe1542799177faa6ce5ba12827909
Ossama W. Obeid <owo@owobeid.com>
2018-11-27 16:27:23 +0400

    Fixed version in docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ author = 'Ossama W. Obeid'
 # The short X.Y version
 version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0b'
+release = '0.1.0dev'
 
 
 # -- General configuration ---------------------------------------------------
