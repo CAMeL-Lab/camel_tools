3730b2d5c58e3c67630223be2ce32107801f0925
Ossama W. Obeid <owo@owobeid.com>
2018-12-04 18:07:59 +0400

    Version is now consistent with what PyPi displays.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,9 +27,9 @@ copyright = '2018, New York University Abu Dhabi'
 author = 'Ossama W. Obeid'
 
 # The short X.Y version
-version = '0.1-dev'
+version = '0.1.dev'
 # The full version, including alpha/beta/rc tags
-release = '0.1-dev'
+release = '0.1.dev0'
 
 
 # -- General configuration ---------------------------------------------------
