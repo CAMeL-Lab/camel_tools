1dbc7304bfcf6b004aeb83a34c247163224d5f70
Ossama W. Obeid <owo@owobeid.com>
2019-09-12 14:56:35 +0400

    Version bump to 0.4.dev2.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ author = 'Ossama W. Obeid'
 # The short X.Y version
 version = '0.4'
 # The full version, including alpha/beta/rc tags
-release = '0.4.dev1'
+release = '0.4.dev2'
 
 
 # -- General configuration ---------------------------------------------------
