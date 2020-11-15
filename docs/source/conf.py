1751f1a1818f12fd383cf9daaa56caba08e146cc
Ossama W. Obeid <owo@owobeid.com>
2020-11-15 10:57:01 +0400

    Bumped version to 1.0.1.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ author = 'Ossama W. Obeid'
 # The short X.Y version
 version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
