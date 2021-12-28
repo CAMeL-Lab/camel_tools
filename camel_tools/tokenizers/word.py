c26947b53ea93526099a1291ebebc52902fb7ff9
Ossama W. Obeid <owo@owobeid.com>
2021-12-28 13:33:43 +0400

    simple_word_tokenize now respects multi-character emoji sequences.

--- camel_tools/tokenizers/word.py
+++ camel_tools/tokenizers/word.py
@@ -34,26 +34,34 @@ from camel_tools.utils.charsets import UNICODE_LETTER_MARK_NUMBER_CHARSET
 from camel_tools.utils.charsets import UNICODE_LETTER_CHARSET
 from camel_tools.utils.charsets import UNICODE_MARK_CHARSET
 from camel_tools.utils.charsets import UNICODE_NUMBER_CHARSET
+from camel_tools.utils.charsets import EMOJI_MULTICHAR_CHARSET
 
 
-_ALL_PUNCT = u''.join(UNICODE_PUNCT_SYMBOL_CHARSET)
+__all__ = ['simple_word_tokenize']
+
+
+_ALL_PUNCT_SYMBOLS = (UNICODE_PUNCT_SYMBOL_CHARSET | EMOJI_MULTICHAR_CHARSET)
+_ALL_PUNCT_SYMBOLS = [re.escape(x) for x in _ALL_PUNCT_SYMBOLS]
+_ALL_PUNCT_SYMBOLS = sorted(_ALL_PUNCT_SYMBOLS, key=len, reverse=True)
+
 _ALL_NUMBER = u''.join(UNICODE_NUMBER_CHARSET)
 _ALL_LETTER_MARK = u''.join((UNICODE_LETTER_CHARSET | UNICODE_MARK_CHARSET))
 _ALL_LETTER_MARK_NUMBER = u''.join(UNICODE_LETTER_MARK_NUMBER_CHARSET)
 
-_TOKENIZE_RE = re.compile(r'[' + re.escape(_ALL_PUNCT) + r']|[' +
+_TOKENIZE_RE = re.compile(u'|'.join(_ALL_PUNCT_SYMBOLS) + r'|[' +
                           re.escape(_ALL_LETTER_MARK_NUMBER) + r']+')
-_TOKENIZE_NUMBER_RE = re.compile(r'[' + re.escape(_ALL_PUNCT) + r']|[' +
+_TOKENIZE_NUMBER_RE = re.compile(u'|'.join(_ALL_PUNCT_SYMBOLS) + r'|[' +
                                  re.escape(_ALL_NUMBER) + r']+|[' +
                                  re.escape(_ALL_LETTER_MARK) + r']+')
 
 
 def simple_word_tokenize(sentence, split_digits=False):
     """Tokenizes a sentence by splitting on whitespace and seperating
-    punctuation. The resulting tokens are either alpha-numeric words or single
-    punctuation/symbol characters. This function is language agnostic and
-    splits all characters marked as punctuation or symbols in the Unicode
-    specification. For example, tokenizing :code:`'Hello,    world!!!'`
+    punctuation. The resulting tokens are either alpha-numeric words, single
+    punctuation/symbol/emoji characters, or multi-character emoji sequences.
+    This function is language agnostic and splits all characters marked as
+    punctuation or symbols in the Unicode specification.
+    For example, tokenizing :code:`'Hello,    world!!!'`
     would yield :code:`['Hello', ',', 'world', '!', '!', '!']`.
     If split_digits is set to True, it also splits on number.
     For example, tokenizing :code:`'Hello,    world123!!!'`
