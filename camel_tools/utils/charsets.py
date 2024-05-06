2e253c5f0a84b03ca1e9cd65ea6ea673998c84f3
Ossama W. Obeid <owo@owobeid.com>
2024-05-06 08:44:41 +0400

    Tatweel character no longer considered a diacritic.

--- camel_tools/utils/charsets.py
+++ camel_tools/utils/charsets.py
@@ -89,26 +89,26 @@ AR_LETTERS_CHARSET = frozenset(u'\u0621\u0622\u0623\u0624\u0625\u0626\u0627'
                                u'\u0642\u0643\u0644\u0645\u0646\u0647\u0648'
                                u'\u0649\u064a\u0671\u067e\u0686\u06a4\u06af')
 AR_DIAC_CHARSET = frozenset(u'\u064b\u064c\u064d\u064e\u064f\u0650\u0651\u0652'
-                            u'\u0670\u0640')
+                            u'\u0670')
 AR_CHARSET = AR_LETTERS_CHARSET | AR_DIAC_CHARSET
 
-BW_LETTERS_CHARSET = frozenset(u'$&\'*<>ADEGHJPSTVYZ_bdfghjklmnpqrstvwxyz{|}')
-BW_DIAC_CHARSET = frozenset(u'FKN`aiou~_')
+BW_LETTERS_CHARSET = frozenset(u'$&\'*<>ADEGHJPSTVYZ_bdfghjklmnpqrstvwxyz{|}_')
+BW_DIAC_CHARSET = frozenset(u'FKN`aiou~')
 BW_CHARSET = BW_LETTERS_CHARSET | BW_DIAC_CHARSET
 
 SAFEBW_LETTERS_CHARSET = frozenset(u'ABCDEGHIJLMOPQSTVWYZ_bcdefghjklmnpqrstvwx'
                                    u'yz')
-SAFEBW_DIAC_CHARSET = frozenset(u'FKNaeiou~_')
+SAFEBW_DIAC_CHARSET = frozenset(u'FKNaeiou~')
 SAFEBW_CHARSET = SAFEBW_LETTERS_CHARSET | SAFEBW_DIAC_CHARSET
 
 XMLBW_LETTERS_CHARSET = frozenset(u'$\'*ABDEGHIJOPSTWYZ_bdfghjklmnpqrstvwxyz{|'
                                   u'}')
-XMLBW_DIAC_CHARSET = frozenset(u'FKN`aiou~_')
+XMLBW_DIAC_CHARSET = frozenset(u'FKN`aiou~')
 XMLBW_CHARSET = XMLBW_LETTERS_CHARSET | XMLBW_DIAC_CHARSET
 
 HSB_LETTERS_CHARSET = frozenset(u'\'ADHST_bcdfghjklmnpqrstvwxyz'
                                 u'\u00c2\u00c4\u00e1\u00f0\u00fd\u0100\u0102'
                                 u'\u010e\u0127\u0161\u0175\u0177\u03b3\u03b8'
                                 u'\u03c2')
-HSB_DIAC_CHARSET = frozenset(u'.aiu~\u00c4\u00e1\u00e3\u0129\u0169_')
+HSB_DIAC_CHARSET = frozenset(u'.aiu~\u00c4\u00e1\u00e3\u0129\u0169')
 HSB_CHARSET = HSB_LETTERS_CHARSET | HSB_DIAC_CHARSET
