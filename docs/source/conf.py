17b4d31c4d1909f05dafeaf7e147f747bec31256
Ossama W. Obeid <owo@owobeid.com>
2025-04-15 22:17:09 +0400

    Bumped version to 1.5.6.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ author = 'Ossama W. Obeid'
 # The short X.Y version
 version = '1.5'
 # The full version, including alpha/beta/rc tags
-release = '1.5.5'
+release = '1.5.6'
 
 
 # -- General configuration ---------------------------------------------------
